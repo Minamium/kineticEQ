a32aaf556e447f0cd4f49857f7b157befae83677
Minamium <arcadia.battery@gmail.com>
2025-07-24 18:22:40 +0900

    edit __init__.py for build

--- src/kineticEQ/__init__.py
+++ src/kineticEQ/__init__.py
@@ -1,3 +1,8 @@
 from . import BGK1Dsim
 from . import progress_bar
-from . import gtsv_binding
+
+# gtsv_binding はビルドできたときだけ import
+try:
+    from . import gtsv_binding
+except ImportError as e:
+    print("[kineticEQ] gtsv_binding not available:", e)
