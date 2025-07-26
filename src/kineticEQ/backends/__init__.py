989aaf2f313c8516fb9e3265912e19cceedee7b9
Minamium <arcadia.battery@gmail.com>
2025-07-26 22:22:27 +0900

    wheel関係のファイルとgitignoreの編集

--- src/kineticEQ/backends/__init__.py
+++ src/kineticEQ/backends/__init__.py
@@ -1,7 +1,9 @@
 from importlib import import_module
+
 try:
-    advection1d = import_module("kineticEQ.backends.advection1d")
-    advect_upwind = advection1d.sample_1d_advection_module.advec_upwind_step
+    _mod = import_module("kineticEQ.backends.advection1d")
+    advect_upwind = _mod.sample_1d_advection_module.advec_upwind_step
 except ModuleNotFoundError:
     advect_upwind = None
+
 __all__ = ["advect_upwind"]
