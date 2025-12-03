6372fe148e0fa6fe18a67f541d53a39beefee5a6
Minamium <arcadia.battery@gmail.com>
2025-12-04 07:40:41 +0900

    実験

--- src/kineticEQ/BGK1Dsim.py
+++ src/kineticEQ/BGK1Dsim.py
@@ -1763,9 +1763,9 @@ class BGK1D:
         f_up[:, self._neg_mask] = fR[:, self._neg_mask]
 
         # 界面フラックス
-        w1 = 0.0 * torch.ones_like(v)  # 1
-        w2 = 0.0 * v                   # v
-        w3 = 0.0 * 0.5 * v * v         # 0.5 v^2
+        w1 = v                      # 1
+        w2 = v * v                  # v
+        w3 = 0.5 * v * v * v        # 0.5 v^2
 
         S_1_HO = torch.sum(f_up * w1[None, :], dim=1) * dv  # (nx-1,)
         S_2_HO = torch.sum(f_up * w2[None, :], dim=1) * dv  # (nx-1,)
