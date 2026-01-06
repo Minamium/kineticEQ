f9044af39f42b2d73f5d4fe9aa75e198d2ecdc52
Minamium <arcadia.battery@gmail.com>
2026-01-06 09:08:19 +0900

    修正

--- src/kineticEQ/__init__.py
+++ src/kineticEQ/__init__.py
@@ -1,12 +1,9 @@
 from . import BGK1Dsim
 from . import progress_bar
 from . import BGK1DPlotUtil
-from . import BGK1D_old
 from . import BGK2D2V_core
 
-# 便利なエイリアス
+# エイリアス
 from .BGK1Dsim import BGK1D
 from .BGK1DPlotUtil import BGK1DPlot
-from .BGK1D_old import BGK1D_old
-from .BGK1DPlotUtil import BGK1DPlot_old
 from .BGK2D2V_core import BGK2D2V_core
