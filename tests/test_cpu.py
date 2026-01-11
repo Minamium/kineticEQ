2db583c371b85378f6d11a9d2a1e5c1579df33b0
Minamium <arcadia.battery@gmail.com>
2026-01-12 00:34:49 +0900

    パラメータの記法を修正

--- tests/test_cpu.py
+++ tests/test_cpu.py
@@ -4,10 +4,24 @@ from kineticEQ import Config, Engine
 @pytest.mark.parametrize("model", ["BGK1D1V"])
 @pytest.mark.parametrize("scheme", ["explicit"])
 def test_smoke_cpu(model, scheme):
-    cfg = Config(model=model, scheme=scheme, backend="torch", device="cpu", use_tqdm="false", log_level="DEBUG",
-                nx=124, nv=64, Lx=1.0, v_max=10.0, dt=5e-4, T_total=5e-3, tau_tilde=5e-1,
+    cfg = Config(
+        model=model,
+        scheme=scheme,
+        backend="torch",
+        device="cpu",
+        dtype="float64",
+        use_tqdm="false",
+        log_level="debug",
+        model_cfg=ModelConfig(
+            grid=Grid1D1V(nx=124, nv=64, Lx=1.0, v_max=10.0),
+            time=TimeConfig(dt=5e-4, T_total=5e-3),
+            params=BGK1D1VParams(tau_tilde=5e-1),
+            initial=InitialCondition1D(
                 initial_regions=(
                     {"x_range": (0.0, 0.5), "n": 1.0, "u": 0.0, "T": 1.0},
                     {"x_range": (0.5, 1.0), "n": 0.125, "u": 0.0, "T": 0.8},
-                ))
+                )
+            ),
+        ),
+    )
     Engine(cfg, apply_logging_flag=True).run()
--- tests/test_cuda.py
+++ tests/test_cuda.py
@@ -12,5 +12,24 @@ def test_resolve_device_cuda_ok():
 @pytest.mark.parametrize("scheme", ["explicit"])
 @pytest.mark.parametrize("backend", ["torch"])
 def test_smoke_cuda(model, scheme, backend):
-    cfg = Config(model=model, scheme=scheme, backend=backend, device="cuda", use_tqdm="false", log_level="DEBUG")
+    cfg = Config(
+        model=model,
+        scheme=scheme,
+        backend=backend,
+        device="cuda",
+        dtype="float64",
+        use_tqdm="false",
+        log_level="debug",
+        model_cfg=ModelConfig(
+            grid=Grid1D1V(nx=124, nv=64, Lx=1.0, v_max=10.0),
+            time=TimeConfig(dt=5e-4, T_total=5e-3),
+            params=BGK1D1VParams(tau_tilde=5e-1),
+            initial=InitialCondition1D(
+                initial_regions=(
+                    {"x_range": (0.0, 0.5), "n": 1.0, "u": 0.0, "T": 1.0},
+                    {"x_range": (0.5, 1.0), "n": 0.125, "u": 0.0, "T": 0.8},
+                )
+            ),
+        ),
+    )
     Engine(cfg, apply_logging_flag=True).run()
