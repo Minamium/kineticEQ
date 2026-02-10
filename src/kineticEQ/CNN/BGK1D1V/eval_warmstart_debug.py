7de975f8239ac870115bdb8be09816ebd749fecf
Minamium <arcadia.battery@gmail.com>
2026-02-10 22:13:37 +0900

    BGK1D1V warmstart評価スクリプトのpicard_iterデフォルト値を1000から10000に増加してPicard反復の収束余裕を拡大

--- src/kineticEQ/CNN/BGK1D1V/eval_warmstart_debug.py
+++ src/kineticEQ/CNN/BGK1D1V/eval_warmstart_debug.py
@@ -228,7 +228,7 @@ def parse_args():
     p.add_argument("--Lx", type=float, default=1.0)
     p.add_argument("--v_max", type=float, default=10.0)
 
-    p.add_argument("--picard_iter", type=int, default=1000)
+    p.add_argument("--picard_iter", type=int, default=10_000)
     p.add_argument("--picard_tol", type=float, default=1e-3)
     p.add_argument("--abs_tol", type=float, default=1e-13)
 
